b9276c9295ad3c0738214a4fdd52bc2e3959a629
Jon Balon <development@jonbalon.com>
2016-06-22 19:21:54 -0400

    Removed backgrounds, removed backgrounds in stylesheets, changed background colour

--- gulpfile.js
+++ gulpfile.js
@@ -18,12 +18,6 @@ var modulesPath = "./node_modules/";
 // Tasks
 ////////////////////////////////////////////////////////////////////////////////
 
-// Move images.
-gulp.task("images", function() {
-  gulp.src(srcPath + "images/**/*")
-    .pipe(gulp.dest(destPath + "images/"));
-});
-
 // Move JavaScript.
 gulp.task("javascript", function() {
   gulp.src(srcPath + "javascript/**/*")
@@ -69,7 +63,6 @@ gulp.task("webserver", function() {
 
 // Watch task
 gulp.task("watch", function() {
-  gulp.watch(srcPath + "images/**/*", ["images"]); // JavaScript.
   gulp.watch(srcPath + "javascript/**/*", ["javascript"]); // JavaScript.
   gulp.watch(srcPath + "stylesheets/**/*.scss", ["stylesheets"]); // SASS Main.
   gulp.watch(srcPath + "stylesheets/**/_*.scss", ["stylesheets"]); // SASS Partials.
@@ -79,4 +72,4 @@ gulp.task("watch", function() {
 ////////////////////////////////////////////////////////////////////////////////
 // Default Task.
 ////////////////////////////////////////////////////////////////////////////////
-gulp.task("default", ["images", "javascript", "stylesheets", "html", "components", "watch", "webserver"]);
+gulp.task("default", ["javascript", "stylesheets", "html", "components", "watch", "webserver"]);
