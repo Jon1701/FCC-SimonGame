7cfe2a4ba5434a7c48c852670b6c7d58b6f6ec6a
Jon Balon <development@jonbalon.com>
2016-06-22 03:41:46 -0400

    UI changed

--- source/javascript/simon.js
+++ source/javascript/simon.js
@@ -1,3 +1,49 @@
 $(document).ready(function() {
+  /*
+  // Button click.
+  $(".btn").on("click", function() {
+
+
+    if (id === "green") {
+      inactiveClass = "btn-green-inactive";
+      activeClass = "btn-green-active";
+    } else if (id === "red") {
+      inactiveClass = "btn-red-inactive";
+      activeClass = "btn-red-active";
+    } else if (id === "yellow") {
+      inactiveClass = "btn-yellow-inactive";
+      activeClass = "btn-yellow-active";
+    } else {
+      inactiveClass = "btn-blue-inactive";
+      activeClass = "btn-blue-active";
+    }
+
+    $(this).mousedown(function() {console.log(activeClass);});
+    $(this).mouseup(function() {console.log(inactiveClass);});
+
+  });
+  */
+
+  /*
+  $(".btn").mousedown(function() {
+
+    // Get the colour of the button which was just pressed.
+    var btnColour = $(this).attr("id").split("-")[1];
+
+    console.log(btnColour);
+  });
+
+  $(".btn").mouseup(function() {console.log("inactive");});
+  */
+
+  $(".btn").on("click", function() {
+    $(this).removeClass("btn-green-inactive");
+    $(this).addClass("btn-green-active");
+
+    $(this).removeClass("btn-green-active");
+        $(this).addClass("btn-green-inactive");
+  });
+
+
 
 });
